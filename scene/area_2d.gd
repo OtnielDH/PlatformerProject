955e9aec16460fa98d9f1868465c193b71ec9034
Otniel Dwimartin Hutapea <otnieldwimartin21@gmail.com>
2024-09-14 21:09:54 +0700

    feat : tilemap, killzone , camera
    tilemap : temporary grass terrain, spike and background object
    killzone : implement killzone using area 2D
    camera : implement camera movement using lerp by player post

---
+++ scene/area_2d.gd
@@ -0,0 +1,26 @@
+extends Area2D
+
+#on ready is to get fixed other node in the same tree
+#export let developer to choose which node want to be input to script
+@onready var timer: Timer = $Timer
+@export var player: CharacterBody2D
+
+
+# when player have collision with the killzone
+func _on_body_entered(player) -> void:
+	print("GAME OVER!!!")
+	# slow the time by a half
+	Engine.time_scale = 0.5
+	# remove node (player) from scene
+	player.queue_free()
+	# timer given signal to start counting
+	timer.start()
+
+
+# when time waiting duration is over 
+func _on_timer_timeout() -> void:
+	print("GAME OVER!!!")
+	# return time normally
+	Engine.time_scale = 1.0
+	# reload the scene from beginning
+	get_tree().reload_current_scene()
---
+++ scene/game_camera.gd
@@ -0,0 +1,27 @@
+extends Camera2D
+
+# variable that contain vector (x, y)
+# zero = (0,0)
+var target_position = Vector2.ZERO
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	make_current()
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	# it will continously get player position
+	acquire_target()
+	# made the camera position change with lerp calculation
+	global_position = global_position.lerp(target_position, 1.0 - exp(-delta * 500))
+
+
+# function to get player position from vector
+func acquire_target():
+	# will return array in Main tree, that grouped as "player"
+	var player_nodes = get_tree().get_nodes_in_group("player")
+	# if the player is available / not 0
+	if player_nodes.size() > 0:
+		var player = player_nodes[0] as Node2D
+		# assign the player position to target position
+		target_position = player.global_position
